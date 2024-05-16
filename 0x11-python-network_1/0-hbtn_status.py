3906ff76b8e7e8bc3e0559cdef934e78cf0f6059
NAMADOU Moctar Kamakate <kamamoctar@gmail.com>
2024-05-16 07:20:57 +0000

    MaJ 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
+""""
+Fetch 'https://alx-intranet.hbtn.io/status'
 """
-    Script that fetches https://alx-intranet.hbtn.io/status
- """
-
-
 import urllib.request
-"""Importation du module urllib"""
-
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        status = response.read()
-
-        print('Body response:')
-        print("\t- type: {}".format(type(status)))
-        print("\t- content: {}".format(status))
-        print("\t- utf8 content: {}".format(status.decode('utf8')))
+        html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
