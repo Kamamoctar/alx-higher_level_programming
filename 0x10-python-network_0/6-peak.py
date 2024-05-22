fdaba82d8e8b87dc9ead5f4b4068ec5ef3bec2d9
Moctar K. NAMADOU <kamamoctar@gmail.com>
2024-05-22 15:41:50 +0000

    Refait

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,6 @@
 
 
 def find_peak(list_of_integers):
-    """
-    Find the peak integer in a given list of integers.
-
-    Parameters:
-        list_of_integers (list): A list of integers.
-
-    Returns:
-        int or None: The peak integer if found, None if the list is empty.
-    """
     peak_integer = 0
     if len(list_of_integers) == 0:
         return None
